2cb9928ba7b16dac37780a13cc1f56ec485feedd
VgTajdd <aduranddiaz@gmail.com>
2023-07-07 19:19:19 +0000

    Add tests for ChunkAllocator

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,8 +1,32 @@
 #include <gtest/gtest.h>
 
-#include "pkg/pkg.h"
+#include "univer_pool_allocator/MemoryPoolContainer.h"
 
-TEST( TemplateTest, Test1 )
+TEST( TestChunkAllocator, Capacity )
 {
-	EXPECT_EQ( pkg::getFibonacci( 5 ), 5 );
+	constexpr std::size_t chunkSize{ 256 };
+	constexpr std::size_t halfChunkSize{ chunkSize / 2 };
+	univer::memory::ChunkAllocator<intptr_t, chunkSize> allocator;
+
+	for ( int i = 0; i < halfChunkSize; i++ )
+	{
+		allocator.allocate();
+	}
+
+	float capacity{ (float) allocator.allocatedCount() / allocator.count() };
+
+	EXPECT_EQ( capacity, 0.5f );
+}
+
+TEST( TestChunkAllocator, Exhaustion )
+{
+	constexpr std::size_t chunkSize{ 256 };
+	univer::memory::ChunkAllocator<intptr_t, chunkSize> allocator;
+
+	for ( int i = 0; i < chunkSize; i++ )
+	{
+		EXPECT_NE( allocator.allocate(), nullptr );
+	}
+
+	EXPECT_EQ( allocator.allocate(), nullptr );
 }
