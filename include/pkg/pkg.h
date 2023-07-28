4a9c780ed51188c9e8040403c13e3881e737c39d
VgTajdd <aduranddiaz@gmail.com>
2023-07-28 19:19:19 +0000

    Remove pkg

--- include/pkg/pkg.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace pkg
-{
-void printHelloWorld();
-void printNumber( int number );
-int getFibonacci( int n );
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include "pkg/pkg.h"
-
 #include "univer_pool_allocator/MemoryPoolContainer.h"
 #include "univer_pool_allocator/MemoryPoolAllocator.h"
 #include "univer_pool_allocator/PoolObject.h"
@@ -16,89 +14,84 @@ using univer::memory::PoolObjectRAII;
 
 int main( int, char** )
 {
-	pkg::printHelloWorld();
-	pkg::printNumber( pkg::getFibonacci( 5 ) );
-
 	{
+		auto a{ new PoolObject<Event> };
+		auto b{ new PoolObject<Event> };
+		auto c{ new PoolObject<Event> };
+		delete c;
+		delete b;
+		delete a;
+	}
+	{
+		auto d{ new PoolObject<Event> };
+		auto e{ new PoolObject<Event> };
+		delete d;
+		delete e;
+	}
+	{
+		// auto e{ std::make_shared<PoolObject<Event>>( new PoolObject<Event> ) }; -> Uses placement new.
+		std::shared_ptr<PoolObject<Event>> a( new PoolObject<Event> );
+	}
+	{
+		std::vector < std::shared_ptr<PoolObject<Event>> > pointers;
+		for ( int i = 0; i < 500; i++ )
 		{
-			auto a{ new PoolObject<Event> };
-			auto b{ new PoolObject<Event> };
-			auto c{ new PoolObject<Event> };
-			delete c;
-			delete b;
-			delete a;
-		}
-		{
-			auto d{ new PoolObject<Event> };
-			auto e{ new PoolObject<Event> };
-			delete d;
-			delete e;
-		}
-		{
-			// auto e{ std::make_shared<PoolObject<Event>>( new PoolObject<Event> ) }; -> Uses placement new.
 			std::shared_ptr<PoolObject<Event>> a( new PoolObject<Event> );
+			pointers.push_back( a );
 		}
+	}
+	{
+		MemoryPoolContainer<PoolObject<Event>> allocator;
+		std::shared_ptr<PoolObject<Event>> ptr1;
+	}
+	{
+		MemoryPoolChunk<PoolObject<Event>> allocator;
+		for ( int i = 0; i < 128; i++ )
 		{
-			std::vector < std::shared_ptr<PoolObject<Event>> > pointers;
-			for ( int i = 0; i < 500; i++ )
-			{
-				std::shared_ptr<PoolObject<Event>> a( new PoolObject<Event> );
-				pointers.push_back( a );
-			}
+			allocator.allocate();
 		}
-		{
-			MemoryPoolContainer<PoolObject<Event>> allocator;
-			std::shared_ptr<PoolObject<Event>> ptr1;
-		}
-		{
-			MemoryPoolChunk<PoolObject<Event>> allocator;
-			for ( int i = 0; i < 128; i++ )
-			{
-				allocator.allocate();
-			}
-			allocator.deallocate( nullptr );
-			float capacity{ (float) allocator.allocatedCount() / allocator.capacity() };
-		}
-		{
-			std::vector<Event, MemoryPoolAllocator<Event>> v;
-			v.push_back( Event() );
-		}
-		{
-			std::vector<Event, MemoryPoolAllocator<Event>> v( 8 );
-			v.push_back( Event() );
-		}
-		{
-			MemoryPoolAllocator<Event> allocator;
-			Event* e{ allocator.allocate( 1 ) };
-			allocator.deallocate( e, 1 );
-		}
-		{
-			// std::shared_ptr<PoolObject<Event>> event( new PoolObject<Event> );
-			// std::shared_ptr<PoolObject<Event>> event = std::make_shared<PoolObject<Event>>();
-			// std::unique_ptr<PoolObject<Event>> event = std::make_unique<PoolObject<Event>>();
-			PoolObject<Event>* event = new PoolObject<Event>;
-			event->isHandled();
-			delete event;
-		}
-		{
-			PoolObjectRAII<Event> event;
-			event->isHandled();
-		}
-		{
-			PoolObjectRAII<Event2> event1( false, true );
-			PoolObjectRAII<Event2> event2( false );
-			event1->isHandled();
-			event2->isHandled();
-			event2 = std::move( event1 );
-		}
-		{
-			std::vector<PoolObjectRAII<Event2>> v;
-			v.push_back( PoolObjectRAII<Event2>( false, true ) );
-			v.push_back( PoolObjectRAII<Event2>( false, true ) );
-			v.push_back( PoolObjectRAII<Event2>( false, true ) );
-			v.push_back( PoolObjectRAII<Event2>( false, true ) );
-			const auto& a{ v[0] };
-		}
+		allocator.deallocate( nullptr );
+		float capacity{ (float) allocator.allocatedCount() / allocator.capacity() };
+	}
+	{
+		std::vector<Event, MemoryPoolAllocator<Event>> v;
+		v.push_back( Event() );
+	}
+	{
+		std::vector<Event, MemoryPoolAllocator<Event>> v( 8 );
+		v.push_back( Event() );
+	}
+	{
+		MemoryPoolAllocator<Event> allocator;
+		Event* e{ allocator.allocate( 1 ) };
+		allocator.deallocate( e, 1 );
+	}
+	{
+		// std::shared_ptr<PoolObject<Event>> event( new PoolObject<Event> );
+		// std::shared_ptr<PoolObject<Event>> event = std::make_shared<PoolObject<Event>>();
+		// std::unique_ptr<PoolObject<Event>> event = std::make_unique<PoolObject<Event>>();
+		PoolObject<Event>* event = new PoolObject<Event>;
+		event->isHandled();
+		delete event;
+	}
+	{
+		PoolObjectRAII<Event> event;
+		event->isHandled();
+	}
+	{
+		PoolObjectRAII<Event2> event1( false, true );
+		PoolObjectRAII<Event2> event2( false );
+		event1->isHandled();
+		event2->isHandled();
+		event2 = std::move( event1 );
+	}
+	{
+		std::vector<PoolObjectRAII<Event2>> v;
+		v.push_back( PoolObjectRAII<Event2>( false, true ) );
+		v.push_back( PoolObjectRAII<Event2>( false, true ) );
+		v.push_back( PoolObjectRAII<Event2>( false, true ) );
+		v.push_back( PoolObjectRAII<Event2>( false, true ) );
+		const auto& a{ v[0] };
 	}
 
 	return 0;
