276fb34e2163f799a02032edbb374b095a9dea21
VgTajdd <aduranddiaz@gmail.com>
2023-08-15 19:19:19 +0000

    Add copyright to files

--- example/main.cpp
+++ example/main.cpp
@@ -1,3 +1,9 @@
+// ========================================================================= //
+// Copyright (c) 2023 Agustin Jesus Durand Diaz.                             //
+// This code is licensed under the Apache License 2.0.                       //
+// main.cpp                                                                  //
+// ========================================================================= //
+
 #include "univer_pool_allocator/MemoryPoolContainer.h"
 #include "univer_pool_allocator/MemoryPoolAllocator.h"
 #include "univer_pool_allocator/PoolObject.h"
